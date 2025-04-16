53b4c8a0ec47a22f24c489d26df965d8ebc26851
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2025-04-16 12:16:54 +0100

    t
    tiny

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+class blockCategory {
+	constructor (name, colour) {
+		this.name = name;
+		this.color = color;
+	}
+	
+}
+
 function setProjectTitle(title) {
 	document.title = "Nibble: CTRL " + title;
 }
